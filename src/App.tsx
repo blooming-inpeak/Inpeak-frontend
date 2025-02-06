107587fbaec66a86a2535e0ff589cc32b7ba1d7e
qowldud <bjy6631@daum.net>
2025-02-06 16:03:41 +0900

    페이지 파일

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { MainPage } from './pages/MainPage';
+import { InterviewPage } from './pages/InterviewPage';
+import { HistoryPage } from './pages/HistoryPage';
+import { IntroPage } from './pages/interview/IntroPage';
+import { SessionPage } from './pages/interview/SessionPage';
+import { ResultPage } from './pages/interview/ResultPage';
+import { MyPage } from './pages/MyPage';
+import { ContactPage } from './pages/ContactPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
-      <Route path="/interview" />
-      <Route path="/history" />
-      <Route path="/interview/intro" />
-      <Route path="/interview/session" />
-      <Route path="/interview/result" />
-      <Route path="/mypage" />
-      <Route path="/contact" />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/interview" element={<InterviewPage />} />
+      <Route path="/history" element={<HistoryPage />} />
+      <Route path="/interview/intro" element={<IntroPage />} />
+      <Route path="/interview/session" element={<SessionPage />} />
+      <Route path="/interview/result" element={<ResultPage />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/contact" element={<ContactPage />} />
     </Routes>
   );
 }
