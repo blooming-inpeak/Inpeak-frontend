eb28ae103803560c0fa1c3e84064f05cc88d7d68
qowldud <bjy6631@daum.net>
2025-02-06 21:56:00 +0900

    feat: 헤더 컴포넌트 추가 및 기본 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ import { SessionPage } from './pages/interview/SessionPage';
 import { ResultPage } from './pages/interview/ResultPage';
 import { MyPage } from './pages/MyPage';
 import { ContactPage } from './pages/ContactPage';
+import { Header } from './components/common/header/Header';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/interview" element={<InterviewPage />} />
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/interview/intro" element={<IntroPage />} />
-      <Route path="/interview/session" element={<SessionPage />} />
-      <Route path="/interview/result" element={<ResultPage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/contact" element={<ContactPage />} />
+      <Route element={<Header />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/interview/intro" element={<IntroPage />} />
+        <Route path="/interview/session" element={<SessionPage />} />
+        <Route path="/interview/result" element={<ResultPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Route>
     </Routes>
   );
 }
