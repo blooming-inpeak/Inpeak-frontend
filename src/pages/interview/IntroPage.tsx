04e6ab64a88405bd5cdf86785269fb573f7fa922
qowldud <bjy6631@daum.net>
2025-02-16 00:42:04 +0900

    feat: interviewIntro 설명 컴포넌트

--- src/pages/interview/IntroPage.tsx
+++ src/pages/interview/IntroPage.tsx
@@ -1,3 +1,33 @@
+import styled from 'styled-components';
+import { IntroDescription } from '../../components/intro/IntroDescription';
+
 export const IntroPage = () => {
-  return <div>IntroPage</div>;
+  return (
+    <IntroWrapper>
+      <IntroBody>
+        <IntroDescription></IntroDescription>
+      </IntroBody>
+    </IntroWrapper>
+  );
 };
+
+export const IntroWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const IntroBody = styled.div`
+  width: 698px;
+  height: 563px;
+
+  border-radius: 24px;
+  background-color: rgba(255, 255, 255, 0.6);
+  box-shadow: 100px 100px 100px 0px rgba(0, 0, 0, 0.02), 2px 4px 4px 0px rgba(255, 255, 255, 0.24) inset,
+    0px 0px 100px 0px rgba(0, 80, 216, 0.08);
+
+  display: flex;
+`;
