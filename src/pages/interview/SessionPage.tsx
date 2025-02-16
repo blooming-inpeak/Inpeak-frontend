47b3ef64d959cc9fe62ea41b34f5ea856b9db65c
qowldud <bjy6631@daum.net>
2025-02-16 15:20:07 +0900

    feat: layout 초반 구현

--- src/pages/interview/SessionPage.tsx
+++ src/pages/interview/SessionPage.tsx
@@ -1,3 +1,29 @@
+import styled from 'styled-components';
+
 export const SessionPage = () => {
-  return <div>SessionPage</div>;
+  return (
+    <SessionWrapper>
+      <SessionBody></SessionBody>
+    </SessionWrapper>
+  );
 };
+
+export const SessionWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SessionBody = styled.div`
+  width: 600px;
+  height: 600px;
+  border-radius: 24px;
+
+  display: flex;
+  flex-direction: column;
+  box-shadow: 100px 100px 100px 0px rgba(0, 0, 0, 0.02), 2px 4px 4px 0px rgba(255, 255, 255, 0.24) inset,
+    0px 0px 100px 0px rgba(0, 80, 216, 0.08);
+`;
