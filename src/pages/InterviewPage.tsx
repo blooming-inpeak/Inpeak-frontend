5464c8ae9b67821676890b2380fb859f70129d42
paicearea <qocodms20010302@gmail.com>
2025-02-21 22:34:32 +0900

    feat: 인터뷰 페이지 상단 배너 ui 개발 완료

--- src/pages/InterviewPage.tsx
+++ src/pages/InterviewPage.tsx
@@ -4,22 +4,28 @@ import { InterviewChance } from '../components/interview/InterviewChance';
 import { Strict } from '../components/interview/Strict';
 import { AskHistory } from '../components/interview/AskHistory';
 import BackgroundImage from '../assets/img/background.svg';
-
+import { Link } from 'react-router-dom';
 
 export const InterviewPage = () => {
   return (
     <InterviewWrapper>
       <InterviewTop>
-        <CorrectAnswer />
-        <InterviewTopContent>
+        <div id="bannerTop">
+          <CorrectAnswer />
           <InterviewChance />
-          <InterviewButton>
+        </div>
+        <div id="bannerBottom">
+          <div id="bannerBottomtext">
+            <h2>기술면접 AI통해 피드백 받고</h2>
+            <h1>취업까지 10발자국 다가가자</h1>
+          </div>
+          <InterviewButton to="/interview/intro">
             <InterviewButtonTitle>모의면접 연습하기</InterviewButtonTitle>
             <img src="/images/chevron/Chevron_right_white.svg" alt="chevron right" />
           </InterviewButton>
-        </InterviewTopContent>
+        </div>
       </InterviewTop>
-      
+
       <InterviewBottom>
         <InterviewBottomLeft>
           <Strict />
@@ -34,13 +40,11 @@ export const InterviewPage = () => {
 
 export const InterviewWrapper = styled.div`
   width: 100%;
-  height: 100%;
   min-height: 1178px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 60px;
+  padding: 0 16px;
 
   &::before {
     content: '';
@@ -51,46 +55,75 @@ export const InterviewWrapper = styled.div`
     height: 544px;
     z-index: -1;
     background-image: url(${BackgroundImage});
-    background-size: cover;
-    background-repeat: no-repeat;
     background-position: center;
   }
 `;
 
 export const InterviewTop = styled.div`
-  width: 1128px;
+  width: 100%;
+  max-width: 1128px;
   display: flex;
   flex-direction: column;
-
-  gap: 48px;
   margin-top: 40px;
+
+  #bannerTop {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  #bannerBottom {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-top: 48px;
+  }
+
+  #bannerBottomtext {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+  #bannerBottomtext h1,
+  #bannerBottomtext h2 {
+    margin: 0;
+    padding: 0;
+  }
+
+  h1 {
+    font-size: 40px;
+    letter-spacing: -0.2px;
+  }
+
+  h2 {
+    font-size: 24px;
+    letter-spacing: -0.12px;
+  }
 `;
 
-export const InterviewTopContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-
-  gap: 12px;
-`;
-
-export const InterviewButton = styled.div`
-  width: 200px;
+export const InterviewButton = styled(Link)`
+  width: 352px;
   height: 44px;
-
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 4px;
-
   border-radius: 100px;
-  background-color: #327eed;
+  background-color: #3277ed;
   cursor: pointer;
+  margin-top: 12px;
+  margin-bottom: 60px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #286fce;
+  }
 `;
 
 export const InterviewButtonTitle = styled.div`
   color: #ffffff;
-
   font-size: 16px;
   font-weight: 600;
   line-height: 150%;
@@ -101,25 +134,22 @@ export const InterviewBottom = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   gap: 23px;
+  width: 100%;
 `;
 
 export const InterviewBottomLeft = styled.div`
   display: flex;
   flex-direction: column;
-
   gap: 20px;
 `;
 
 export const InterviewBottomAd = styled.div`
   width: 456px;
   height: 258px;
-
   border-radius: 24px;
   border: 1px solid #ffffff;
   box-shadow: 0px 8px 24px 0px rgba(0, 80, 216, 0.04);
   backdrop-filter: blur(10px);
-
   background-color: yellow;
 `;
