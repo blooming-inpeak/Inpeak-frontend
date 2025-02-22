260099f1bdce0d0fb6613cfcafeaba54f5d204e1
paicearea <qocodms20010302@gmail.com>
2025-02-23 01:25:28 +0900

    feat: Interview resultpage ui개발 완료

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,13 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/interview" element={<InterviewPage />} />
         <Route path="/history" element={<HistoryPage />} />
-        <Route path="/interview/intro" element={<IntroPage />} />
-        <Route path="/interview/session" element={<SessionPage />} />
-        <Route path="/interview/result" element={<ResultPage />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Route>
+
+      <Route path="/interview/intro" element={<IntroPage />} />
+      <Route path="/interview/session" element={<SessionPage />} />
+      <Route path="/interview/result" element={<ResultPage />} />
     </Routes>
   );
 }
