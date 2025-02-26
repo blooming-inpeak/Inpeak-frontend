b5810254daba2670a536b7ffd8c7a6571f3146a4
qowldud <bjy6631@daum.net>
2025-02-26 17:11:27 +0900

    feat: MainPage Intro Top 컴포넌트 완료

---
+++ src/components/mainpage/intro/MainIntro.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { MainIntroTop } from './MainIntroTop';
+
+export const MainIntro = () => {
+  return (
+    <MainIntroWrapper>
+      <MainIntroTop />
+    </MainIntroWrapper>
+  );
+};
+
+export const MainIntroWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  background: radial-gradient(
+    47.07% 29.79% at 50% 50%,
+    rgba(133, 178, 255, 0.8) 0%,
+    rgba(183, 209, 253, 0.03) 93.5%,
+    rgba(195, 218, 255, 0) 100%
+  );
+`;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+import { MainIntro } from '../components/mainpage/intro/MainIntro';
+
 export const MainPage = () => {
-  return <div></div>;
+  return (
+    <MainPageWrapper>
+      <MainIntro />
+    </MainPageWrapper>
+  );
 };
+
+export const MainPageWrapper = styled.div`
+  height: auto;
+  width: 100vw;
+  overflow: scroll;
+
+  display: flex;
+  flex-direction: column;
+`;
